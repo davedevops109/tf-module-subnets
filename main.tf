62ae16da3d812b4a87acf0fdc8262e9a7af94c62
EPAPRA <dave.devops109@gmail.com>
2023-08-14 19:04:23 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "subnets" {
   availability_zone = var.availability_zone
   cidr_block = var.cidr_block
   default_vpc_id = var.default_vpc_id
-  name = var.name
   env = var.env
+  name = var.name
   vpc_id = var.vpc_id
   vpc_peering_connection_id = var.vpc_peering_connection_id
 }
